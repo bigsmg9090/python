0f8e49e507179b30ebc5c5a63ff1e12b8a7d6077
bigsmg9090 <sgriffin2003@yahoo.com>
2024-04-23 23:39:56 -0500

    Update test_television.py

--- test_television.py
+++ test_television.py
@@ -1,6 +1,7 @@
 from television import *
 import pytest
 
+
 class Test:
 	def setup_method(self):
 		self.t1 = Television()
@@ -13,9 +14,55 @@ class Test:
 	def test_init(self):
 		assert str(self.t1) == "Power = False, Channel = 0, Volume = 0"
 
-	# def test_set_make(self):
-	# 	self.cl.set_make('BMW')
-	# 	assert self.c1.get_make == 'BMW' assert self.c2.get_make() == 'Honda'
-	# def test_str(self):
-	# 	assert self.cl.__str__( == 'Year = 2000'
-	# 	assert self.c2.__str__( == 'Year = 2000'
+	def test_power(self):
+		self.t1.power()
+		assert str(self.t1) == "Power = True, Channel = 0, Volume = 0"
+		assert str(self.t2) == "Power = False, Channel = 0, Volume = 0"
+
+	def test_mute(self):
+		self.t1.power()
+		self.t1.volume_up()
+		self.t1.mute()
+		assert str(self.t1) == "Power = True, Channel = 0, Volume = 0"
+		self.t2.volume_up()
+		self.t2.volume_up()
+		self.t2.mute()
+		assert str(self.t2) == "Power = False, Channel = 0, Volume = 0"
+
+	def test_channel_change(self):
+		self.t1.power()
+		self.t1.channel_up()
+		assert str(self.t1) == "Power = True, Channel = 1, Volume = 0"
+		self.t2.power()
+		self.t2.channel_down()
+		self.t2.channel_down()
+		assert str(self.t2) == "Power = True, Channel = 2, Volume = 0"
+		self.t1.power()
+		self.t1.channel_up()
+		assert str(self.t1) == "Power = False, Channel = 1, Volume = 0"
+
+	def test_volume_change(self):
+		self.t1.power()
+		self.t1.volume_up()
+		self.t1.volume_up()
+		assert str(self.t1) == "Power = True, Channel = 0, Volume = 2"
+		# mute effects
+		self.t1.mute()
+		assert str(self.t1) == "Power = True, Channel = 0, Volume = 0"
+		self.t1.volume_down()
+		assert str(self.t1) == "Power = True, Channel = 0, Volume = 1"
+		self.t1.mute()
+		assert str(self.t1) == "Power = True, Channel = 0, Volume = 0"
+		self.t1.volume_up()
+		assert str(self.t1) == "Power = True, Channel = 0, Volume = 2"
+		# power off
+		self.t2.volume_up()
+		assert str(self.t2) == "Power = False, Channel = 0, Volume = 0"
+
+
+
+
+
+
+
+
